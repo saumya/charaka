9f180af3e9e8c23814f0428b30e34b09e50a3ae3
saumya <2saumyaray@gmail.com>
2020-02-07 11:32:29 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
+    <!--
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
+    Wow!
   </div>
 </template>
 
 <script>
+/*
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -14,6 +18,7 @@ export default {
     HelloWorld
   }
 }
+*/
 </script>
 
 <style>
