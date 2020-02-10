600fa718f94b4f8e5313814ac7e23873a3fbf61a
saumya <2saumyaray@gmail.com>
2020-02-10 19:05:48 +0530

    working on the details of the components

--- src/components/HomePatient.vue
+++ src/components/HomePatient.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
-        Patient Home
+        
+        <div>
+            <aside class="menu">
+                <p class="menu-label">
+                    Navigate
+                </p>
+                <ul class="menu-list">
+                    <li><a>My History</a></li>
+                    <li><a>New Appointment</a></li>
+                </ul>
+            </aside>
+        </div>
+        <div>
+            Patient Home : TODO
+        </div>
+        
     </div>
 </template>
 
