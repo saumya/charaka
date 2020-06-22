2e69e52701b8dda675d869e69741a45103767927
saumya <2saumyaray@gmail.com>
2020-06-22 11:30:38 +0530

    LoginClinic Home design

--- src/components/ClinicUIActiveHome.vue
+++ src/components/ClinicUIActiveHome.vue
@@ -2,31 +2,33 @@
     <div>
         <h1 class="title is-1"> {{ loggedInClinicData.data.group_name }} </h1>
         <h2 class="subtitle is-6">Active till {{loggedInClinicData.data.activated_to}}</h2>
-        <TableDoctors v-bind:tableData='getDoctorsForTheClinicId'></TableDoctors>
-        
+        <TableDoctors v-bind:tableData='getDoctorsForTheClinicId' />
+        <TableSchedules :tableData='getDataAllSchedulesByClinicId' />
     </div>
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import TableDoctors from './TableDoctors';
+import TableDoctors from './TableDoctors'
+import TableSchedules from './TableSchedules'
 
 export default {
     name: 'ClinicUIActiveHome',
     props: [ 'loggedInClinicData' ],
-    components: { TableDoctors },
+    components: { TableDoctors, TableSchedules },
     created: function(){
         window.console.log('ClinicUIActiveHome : created');
         const payload = this.loggedInClinicData.data.id; 
-        this.$store.dispatch('getAllDoctorsInClinicId', payload );
+        this.$store.dispatch('getAllDoctorsInClinicId', payload )
+        this.$store.dispatch('getAllSchedulesByClinicId', payload)
     },
     mounted: function(){
         window.console.log('ClinicUIActiveHome : mounted');
     },
     computed: {
-        ...mapGetters(['getDoctorsForTheClinicId']),
+        ...mapGetters(['getDoctorsForTheClinicId', 'getDataAllSchedulesByClinicId']),
     },
     methods: {
-        ...mapActions(['getAllDoctorsInClinicId']),
+        ...mapActions(['getAllDoctorsInClinicId', 'getAllSchedulesByClinicId']),
     }
 }
 </script>
